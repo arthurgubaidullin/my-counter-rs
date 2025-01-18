758b6b5efab246d7f6edc6a16226d9515f96846a
Artur Gubaidullin <lapteuh@gmail.com>
2025-01-18 10:59:25 +0000

    Add tailwind to web app

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   server: {
     host: true,
   },
-  integrations: [preact()],
+  integrations: [preact(), tailwind()],
   vite: {
     server: {
       proxy: {
