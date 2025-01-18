de688907702ce92d01024010beb4dd154e25bf8e
Artur Gubaidullin <lapteuh@gmail.com>
2025-01-18 11:13:38 +0000

    Add styles to counter in web app

--- web/src/components/counter.tsx
+++ web/src/components/counter.tsx
@@ -1,14 +1,23 @@
-import { useState } from "preact/hooks";
 import { CounterService } from "../services/counter";
 
 const service = new CounterService();
 
 export const Counter = () => {
   return (
-    <div>
-      <p>{service.value}</p>
+    <div class="grid gap-8 m-8">
+      <div class="text-center">
+        <h1 class="text-5xl">The Counter</h1>
+      </div>
 
-      <button onClick={() => service.increment()}>Increment!</button>
+      <div class="text-center">
+        <span class="font-mono text-9xl">{service.value}</span>
+      </div>
+
+      <div class="text-center">
+        <button class="btn btn-primary" onClick={() => service.increment()}>
+          Increment!
+        </button>
+      </div>
     </div>
   );
 };
